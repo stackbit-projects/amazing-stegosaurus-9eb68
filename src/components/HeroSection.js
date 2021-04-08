edbd2c87894bb6111caaede67904e49773ade331
Stackbit Code Editor <projects@stackbit.com>
2021-04-08 09:30:38 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -76,6 +76,7 @@ export default class HeroSection extends React.Component {
                 			</div>
                 			)}
                         </div>
+                        document.body.appendChild(script);
                 	</div>
                 </section>
             </React.Fragment>
