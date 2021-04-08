0545351c9372aa09e13f94477243f663f1691c5d
Stackbit Code Editor <projects@stackbit.com>
2021-04-08 09:44:04 +0000

    CtaSection.js: changed

--- src/components/CtaSection.js
+++ src/components/CtaSection.js
@@ -62,7 +62,7 @@ export default class CtaSection extends React.Component {
                 				<div className={classNames('cta__actions', 'btn-group', {'justify-md-center': is_horiz, 'justify-center': align_x === 'center', 'justify-end': align_x === 'right'})}>
                 					<SectionActions {...this.props} actions={_.get(section, 'actions', null)} />
                 				</div>
-                                <span><sscript></sscript></span>
+                                <span><script></sscript></span>
                 			</div>
                 			)}
                 		</div>
